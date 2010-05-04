7c1dcbcacee927a97bff9a27bda7907ca9620bc4
Emdek <emdeck@gmail.com>
2010-05-04 09:09:41 +0000

    Set application data

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@
 int main(int argc, char *argv[])
 {
 	QApplication application(argc, argv);
+	application.setApplicationName("WZMapEditor");
+	application.setApplicationVersion("Pre-Alpha");
+	application.setOrganizationName("Warzone2100");
+	application.setOrganizationDomain("wz2100.net");
 
-	WZMapEditor::MainWindow window;
-	window.show();
+	WZMapEditor::MainWindow mainWindow;
+	mainWindow.show();
 
 	return application.exec();
 }
